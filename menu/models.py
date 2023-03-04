cb38504fd7de055e372d6e01849422cf90a7d455
Anubhav Tiwari <anubhavtiwaricp@gmail.com>
2023-03-04 13:31:04 +0530

    menu app

--- menu/models.py
+++ menu/models.py
@@ -5,5 +5,6 @@ class item_Model(models.Model):
     item_Name = models.CharField(max_length=250)
     item_Price = models.IntegerField(blank=True, null=True)
     item_Image = models.ImageField(blank=True, null=True)
+    item_Desc = models.TextField(blank=True, null=True)
     id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
 
